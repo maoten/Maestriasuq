f3f68562a3287c6091a2abeb4b1669fd7899a4cd
Francy Yuliana Zapata <francyzapata119@hotmail.com>
2016-03-05 16:12:03 -0600

    se creó el controlador para el Estudiante

---
+++ app/Http/Controllers/EstudiantesController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Estudiante;
+use Laracasts\Flash\Flash;
+use App\Http\Requests\EstudianteRequest;
+
+
+class EstudiantesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.estudiantes.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.estudiantes.registrar');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(estudianteRequest $request)
+    {
+     $estudiante = new estudiante($request->all());
+     $estudiante->contrasena =bcrypt($request->contrasena);
+     $estudiante->save();
+     Flash::success("Se ha registrado ".$estudiante->nombre." de forma exitosa");
+     return redirect()->route('admin.estudiantes.registrar');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+
+
+    }
+}
